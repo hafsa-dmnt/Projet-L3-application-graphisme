35c8e6a4e52e1146b7d0d6e767cb202d0c3b46e7
Hafsa <62755012+hafsa110@users.noreply.github.com>
2021-12-27 16:57:47 +0100

    update de la page profil

--- app/src/routes/profil.js
+++ app/src/routes/profil.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../App.css';
+import '../CSS/profil.css';
+import profilepicture from '../images/defaultpicture.jpg';
+import { Icon } from '@iconify/react';
 
 import {
   BrowserRouter as Router,
@@ -8,13 +11,53 @@ import {
   Link
 } from "react-router-dom";
 
+class Follow extends React.Component{
+  render(){
+    return(
+        <button id="btnFollow">
+          <Icon icon="heroicons-solid:user-add"/>
+        </button>
+    );
+  }
+}
+
+class ProfilHead extends React.Component{
+  render(){
+    return(
+      <header class="profilHead">
+        <img src={profilepicture} class = "profilePic"></img>
+        <h3>{this.props.pseudo}</h3>
+        <Follow/>
+      </header>
+    );
+  }
+}
+
+class ProfilContent extends React.Component{
+  render(){
+    return(
+      <section class="profilContent">
+        <p><Link to="/profil/mesthemes">Mes thèmes</Link></p>
+        <p><Link to="/profil/mespalettes">Mes Palette</Link></p>
+        <h2>Galerie</h2>
+        <section class="galerie">
+          <div class="publication"></div>
+          <div class="publication"></div>
+          <div class="publication"></div>
+          <div class="publication"></div>
+          <div class="publication"></div>
+          <div class="publication"></div>
+        </section>
+      </section>
+    );
+  }
+}
+
 function Profil() {
     return (
-    <div>
-      <h2>XxGrenouille_du_39</h2>
-      <p><Link to="/profil/mesthemes">Mes thèmes</Link></p>
-      <p>Mes Palette</p>
-      <p>Galerie</p>
+    <div class="profil">
+      <ProfilHead pseudo = "xXGrenouilledu39Xx"/>
+      <ProfilContent />
     </div>
     );
   }
