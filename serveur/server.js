ca20bbb60b1b94831ea7dca5db00ad0b8ad0ce8c
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-01-23 18:55:29 +0100

    port 80

--- serveur/server.js
+++ serveur/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 
 const basedonnee = require('./bd/basedonnee.js');
 
+console.log(port);
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
