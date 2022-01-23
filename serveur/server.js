41e6245cc73b51a48a55ad5b2fa89512ed33abfa
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-01-23 18:58:45 +0100

    port 5432

--- serveur/server.js
+++ serveur/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5432;
 
 const basedonnee = require('./bd/basedonnee.js');
 
