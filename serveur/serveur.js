481a5d552db5bf14cfe441715c6f4cdb39856a9b
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-03 13:17:25 +0100

    Update serveur.js

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+//const port = process.env.PORT || 3001;
 
 const basedonnee = require('./bd/basedonnee.js');
 
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
 
