fa5df5ac79d98bf68ea3328e2c6535892b3841af
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-03 19:39:10 +0100

    Update server.js

--- serveur/server.js
+++ serveur/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+console.log("port", port);
 
 const basedonnee = require('./bd/basedonnee.js');
 
