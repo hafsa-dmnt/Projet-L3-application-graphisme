c840c494e30d8fdbd041fad9142373ca9eb7fc36
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-04 14:32:54 +0100

    update server.js

--- serveur/server.js
+++ serveur/server.js
@@ -6,7 +6,7 @@ console.log("port :", port);
 const basedonnee = require('./bd/basedonnee.js');
 
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/searchUser/:userPseudo', (req, res) => {
