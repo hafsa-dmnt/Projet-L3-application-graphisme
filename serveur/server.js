190df2692b4d4097370be86b24899305ff68f514
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-04 14:12:36 +0100

    install navigate

--- serveur/server.js
+++ serveur/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-console.log("port", port);
+const port = process.env.PORT || 3001;
+console.log("port :", port);
 
 const basedonnee = require('./bd/basedonnee.js');
 
