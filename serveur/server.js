6680696a189c05032b71770b54b7e9653ae5b5fa
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-04 16:01:45 +0100

    Update server.js

--- serveur/server.js
+++ serveur/server.js
@@ -5,6 +5,12 @@ console.log("port :", port);
 
 const basedonnee = require('./bd/basedonnee.js');
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+}
+
 // console.log that your server is up and running
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
 
