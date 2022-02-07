dee682cba15982cdc9852891b52e5341b70c45e7
Almina <marie-almina.gindre@edu.univ-fcomte.fr>
2022-02-07 12:26:56 +0100

    les pays mystiques

--- client/src/routes/creerPublication.js
+++ client/src/routes/creerPublication.js
@@ -5,18 +5,89 @@ import { Icon } from '@iconify/react';
 import {Link} from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
 
 
+function CreateForm(props){
+    const [dateDefi, setDateDefi] = useState(new Date());
+    const [image,setImage] = useState("");
+    const [defi,setDefi] = useState(false);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert('submit');
+    }
+
+    const handleChangeImage = (event) => {
+      const value = URL.createObjectURL(event.target.files[0]);
+      setImage(value);
+    }
+
+    const handleChangeDefi = (event) => {
+      setDefi(event.target.checked);
+    }
+
+
+    //<DatePicker selected={dateDefi} onChange={dateDefi => setDate2(dateDefi)} />
+
+
+    return (
+
+      <form onSubmit={handleSubmit}>
+
+        <div className="section">
+
+          <div className="subSection">
+            <label>
+              <h3>Image :</h3>
+            </label>
+
+            <label htmlFor="file-input" className="label-file">
+              <p>Parcourir...</p>
+            </label>
+
+            <input id="file-input" type="file" accept="image/*" onChange={handleChangeImage}/>
+
+            <img src={image}/>
+
+          </div>
+
+          <div className="subSection">
+            <input type="checkbox"/>
+            <DatePicker selected={dateDefi} onChange={dateDefi => setDateDefi(dateDefi)} locale="es" dateFormat="dd-MM-y"/>
+          </div>
+
+          <div className="subSection">
+            <button type="submit" >
+              <Icon icon="akar-icons:check-box-fill" />
+            </button>
+          </div>
+        </div>
+      </form>
+
+    );
+}
 
 
 function CreerPublication() {
 
-  const [date, setDate] = useState(new Date());
+  /*
+
+    image
+    defi ?
+    date jour courant : auto
+    date defi si defi : datepicker
+    date picker pas dans le futur que dans passé (jusqua quelle date ??)
+    envoyer
+
+  */
 
 
     return (
-        <section className="page">
-            <DatePicker selected={date} onChange={date => setDate(date)} />
+        <section className="page page_creerPubli">
+            <CreateForm/>
 
         </section>
     );
