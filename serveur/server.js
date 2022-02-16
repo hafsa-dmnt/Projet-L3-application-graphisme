d9d7bbcad6b9b2bc5c73152aac565963f415c675
Almina <marie-almina.gindre@edu.univ-fcomte.fr>
2022-02-16 13:59:20 +0100

    Update server.js

--- serveur/server.js
+++ serveur/server.js
@@ -74,4 +74,7 @@ app.get('/themeslist', (req, res) => {
 });
 
 
-app.get('*', (req, res) => res.sendFile(path.resolve('build', '/client/build/index.html'));
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
