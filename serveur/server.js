926c77d080074ee20ea9b2a90c6b73dffc35a6c8
Almina <marie-almina.gindre@edu.univ-fcomte.fr>
2022-02-16 11:21:50 +0100

    ayaya j'ai tout destroy fuite stratégique

--- serveur/server.js
+++ serveur/server.js
@@ -14,7 +14,6 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
-app.use(express.static('build'));
 app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
 
 // console.log that your server is up and running
