26374fc001d415b94022c8725bb63b8f32157d36
Almina <marie-almina.gindre@edu.univ-fcomte.fr>
2022-02-16 11:18:35 +0100

    Tentative de réparation des routes

--- serveur/server.js
+++ serveur/server.js
@@ -14,6 +14,9 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
+app.use(express.static('build'));
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
+
 // console.log that your server is up and running
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
 
