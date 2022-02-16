3dff6a6ae613de988b63ecab31a86f6fa88fbdcb
Hafsa <62755012+hafsa110@users.noreply.github.com>
2022-02-16 12:44:30 +0100

    Update basedonnee.js

--- serveur/bd/basedonnee.js
+++ serveur/bd/basedonnee.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const urlDB = process.env.DATABASE_URL || "postgres://vhaycjrvwoovdh:801dd13e4cb2b375c4940b437ec9fd8db7676d78a9e7039e1bc79b4d6eef5f79@ec2-23-23-199-57.compute-1.amazonaws.com:5432/d7jqv9s53ricm9";
+const urlDB = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString: urlDB,
